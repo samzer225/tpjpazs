f33290be3b2eb5c9a55da9e0ae8865d9cac021f6
Enygeek <61242498+Enygeek@users.noreply.github.com>
2023-02-03 12:11:15 +0100

    add- Methode creatUser & listUser dans la class JpaTest

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -1,10 +1,21 @@
 package jpa;
 
+import system.Ticket;
+import system.User;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 public class JpaTest {
+
+	private EntityManager manager;
+
+	public JpaTest(EntityManager manager) {
+		this.manager = manager;
+	}
+
 
 	/**
 	 * @param args
@@ -29,5 +40,25 @@ public class JpaTest {
 		//		factory.close();
 	}
 
+
+	private void createUser() {
+		int numOfEmployees = manager.createQuery("Select u From User u", User.class).getResultList().size();
+		if (numOfEmployees == 0) {
+			Ticket ticket = new Ticket("bug on project", "Todo");
+			manager.persist(ticket);
+
+			manager.persist(new User("Jakab Gipsz"));
+			manager.persist(new User("Captain Nemo"));
+
+		}
+	}
+
+	private void listUser() {
+		List<User> resultList = manager.createQuery("Select a From User a", User.class).getResultList();
+		System.out.println("num of employess:" + resultList.size());
+		for (User next : resultList) {
+			System.out.println("next employee: " + next);
+		}
+	}
 
 }
